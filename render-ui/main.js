3b9c75997638742066a789f95568cc26f0824dd5
NguyenHung-Hub <daminhnguyenhung@gmail.com>
2022-08-31 14:48:42 +0700

    'push'

---
+++ render-ui/main.js
@@ -0,0 +1,11 @@
+// console.log("page is fully loaded");
+
+// const inputs = document.querySelectorAll(".input");
+// console.log(inputs);
+// inputs.forEach((input) => {
+//     input.addEventListener("focus", () => {
+//         console.log(input.parentNode);
+
+//         input.parentNode.querySelector("label").classList.toggle("focus");
+//     });
+// });
